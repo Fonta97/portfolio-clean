849a8d5a3e2575c5bb49d822e0e6074817fca1d0
Fonta97 <fontana.alessandro97@gmail.com>
2025-06-15 16:21:34 +0200

    test

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -42,7 +42,7 @@ export const links = () => {
 export const meta = () => {
   return baseMeta({
     title: 'Designer + Developer',
-    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+    description: `Design portfolio of ${config.name} — chupa working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
   });
 };
 
